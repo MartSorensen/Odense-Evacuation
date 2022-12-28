4c601157be70c845b9df0e9eff2219d9204e716e
martin <martin.se.sorensen@gmail.com>
2022-12-28 15:45:52 +0100

    evac-app

---
+++ Evacuation/Frontend/src/common/Constants.ts
@@ -0,0 +1,7 @@
+const constansts = {
+    //serverBaseURL: 'https://evacuation-backend-sv6mmvhh4a-ew.a.run.app'
+    //serverBaseURL: 'http://www.maso-evac.com'
+    serverBaseURL: 'https://www.maso-evac.com'
+}
+
+export default constansts;
